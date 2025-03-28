d106561097b9c4cd25de95f0ddd04237e17ac93e
ChongChiSengVincent <60565927+TheEmeraldsPlayer@users.noreply.github.com>
2025-03-29 00:51:18 +0800

    Create GridManager

---
+++ Assets/Scripts/Game/GridManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+/// <summary>
+/// Class that handles spawning and snapping of units to their respective positions in world position
+/// </summary>
+public class GridManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
