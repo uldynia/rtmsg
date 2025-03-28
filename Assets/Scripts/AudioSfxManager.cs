ed7b67f27c42a4dc362eefd34973ca23c64f015c
TerYeeHong <130955170+TerYeeHong@users.noreply.github.com>
2025-03-29 00:15:36 +0800

    DragDropMergeSpawn

---
+++ Assets/Scripts/AudioSfxManager.cs
@@ -0,0 +1,160 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSfxManager : MonoBehaviour
+{
+    public static AudioSfxManager m_instance = null;
+
+    [Header("References")]
+    [SerializeField] Transform listener_transform;
+    [SerializeField] UnityEngine.Audio.AudioMixerGroup output_group = null;
+
+    [Header("Attributes")]
+    [SerializeField] int audio_source_init_amount = 10;
+    [SerializeField] float distance_per_fade_ratio = 2; //How many distance for every 10% volume fade
+    [SerializeField] float distance_start_fade = 2; //How far till distance starts to fade
+
+    List<AudioSource> audio_sources;
+
+    //Store the current audio data
+    SFX_TYPE sfx_type_current;
+    Vector3 source_position_current;
+
+    public void SetListener(Transform transform)
+    {
+        listener_transform = transform;
+    }
+    private void Awake()
+    {
+        audio_sources = new();
+        if (m_instance == null)
+            m_instance = this;
+
+        //Create a set number of audio sources at the beginning first
+        for (int i = 0; i < audio_source_init_amount; ++i)
+        {
+            CreateNewAudioSource();
+        }
+
+    }
+    private void OnEnable()
+    {
+        //GameEvents.m_instance.playNewAudioClip.AddListener(OnPlayNewAudioClip);
+        //GameEvents.m_instance.playNewAudioClip3D.AddListener(OnPlayNewAudioClip3D);
+    }
+    private void OnDisable()
+    {
+        //GameEvents.m_instance.playNewAudioClip.RemoveListener(OnPlayNewAudioClip);
+        //GameEvents.m_instance.playNewAudioClip3D.RemoveListener(OnPlayNewAudioClip3D);
+    }
+
+    public enum SFX_TYPE
+    {
+        _2D = 0,
+        _3D,
+        NUM_TOTAL,
+    }
+
+
+    /// <summary>
+    /// LISTENERS TO GAME EVENTS
+    /// </summary>
+    public void OnPlayNewAudioClip(AudioClip audio_clip)
+    {
+        //Set the attributes for new audio clip
+        sfx_type_current = SFX_TYPE._2D;
+
+        //Play the audio clip
+        SearchAndPlayAudioClip(audio_clip);
+    }
+    public void OnPlayNewAudioClip3D(AudioClip audio_clip, Vector3 source_position)
+    {
+        //Set the attributes for new audio clip
+        sfx_type_current = SFX_TYPE._3D;
+        source_position_current = source_position;
+
+        //Play the audio clip
+        SearchAndPlayAudioClip(audio_clip);
+    }
+    /// <summary>
+    /// LISTENERS TO GAME EVENTS END
+    /// </summary>
+
+
+    void SearchAndPlayAudioClip(AudioClip audio_clip)
+    {
+        //Loop thru all audiosources, find one thats not playing anything, use that to play the audio
+        foreach (AudioSource audio_source in audio_sources)
+        {
+            //If this audio source is available
+            if (!audio_source.isPlaying)
+            {
+                //Play clip
+                PlayAudio(audio_source, audio_clip);
+                return; //return earlu
+            }
+        }
+
+        //An available audio source is not found, create a new one
+        PlayAudio(CreateNewAudioSource(), audio_clip);
+    }
+
+
+
+    //Play Sfx
+    void PlayAudio(AudioSource audio_source, AudioClip audio_clip)
+    {
+        //Check which audio type it is
+        switch (sfx_type_current)
+        {
+            //Play 2D Audio
+            case SFX_TYPE._2D:
+                audio_source.clip = audio_clip;
+                audio_source.volume = 1;
+                audio_source.Play();
+                break;
+
+            //Play 3D audio
+            case SFX_TYPE._3D:
+                //Calculate distance
+                float distance = (listener_transform.position - source_position_current).magnitude;
+
+                //Calculate volume 
+                float volume = 1;
+                distance -= distance_start_fade;
+                while (distance > distance_per_fade_ratio)
+                {
+                    volume -= 0.1f;
+                    distance -= distance_per_fade_ratio;
+                }
+                if (volume < 0)
+                    volume = 0;
+
+                //Play
+                audio_source.clip = audio_clip;
+                audio_source.volume = volume;
+                audio_source.Play();
+                break;
+        }
+
+        return;
+    }
+
+
+    AudioSource CreateNewAudioSource()
+    {
+        //Instantiate new audio source and add to list
+        AudioSource new_audio_source = gameObject.AddComponent<AudioSource>();
+        new_audio_source.playOnAwake = false;
+        new_audio_source.loop = false;
+        new_audio_source.outputAudioMixerGroup = output_group;
+
+        //Add to list
+        audio_sources.Add(new_audio_source);
+
+        //Return
+        return new_audio_source;
+    }
+
+}
---
+++ Assets/Scripts/DragDropUI/InventorySlot.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    [Header("References")]
+    public InventoryItem my_inventory_item; //for convenient access
+    public Image image;
+    public Color selected_color, not_selected_color;
+
+    bool selected = false;
+
+
+    private void Awake()
+    {
+        my_inventory_item = transform.GetChild(0).GetComponent<InventoryItem>();
+        Deselect();
+    }
+
+    public void Select() {
+        image.color = selected_color;
+        selected = true;
+    }
+
+    public void Deselect()
+    {
+        image.color = not_selected_color;
+        selected = false;
+    }
+
+
+    public void OnClick()
+    {
+        if (selected) {
+            Deselect();
+            GameObject.Find("InventoryManager").GetComponent<InventoryManager>().ChangeSelectedSlot(null);
+        }
+        else {
+            Select();
+            GameObject.Find("InventoryManager").GetComponent<InventoryManager>().ChangeSelectedSlot(this);
+        }
+           
+    }
+
+    //Drag and Drop
+    public void OnDrop(PointerEventData eventData)
+    {
+        InventoryItem other_inventory_item = eventData.pointerDrag.GetComponent<InventoryItem>();
+
+        //Cannot merge with own tile
+        if (my_inventory_item == other_inventory_item)
+        {
+            return; //IGNORE
+        }
+        //If both empty, ignore
+        if (my_inventory_item.animal_type == null && other_inventory_item.animal_type == null)
+        {
+            return; //IGNORE
+        }
+        //If Empty, Move there
+        if ((my_inventory_item.animal_type != null && other_inventory_item.animal_type == null)
+            || (my_inventory_item.animal_type == null && other_inventory_item.animal_type != null))
+        {
+            AnimalType type = my_inventory_item.animal_type;
+            my_inventory_item.InitialiseItem(other_inventory_item.animal_type);
+            other_inventory_item.InitialiseItem(type);
+        }
+        //If Can Merge, Merge
+        else if (my_inventory_item.animal_type.CanMergeWith(other_inventory_item.animal_type, out AnimalType result))
+        {
+            //SPECIAL CASE
+            //Check for the special case which is sheep, and create a instance of scriptable with improved stats
+            if (result.Name == "Cotton Ball of Sheeps")
+            {
+                //Create a clone n alter the clone's base stats
+                result = Instantiate(result);
+                result.AddHealth(2);
+            }
+
+            my_inventory_item.InitialiseItem(result);
+            other_inventory_item.InitialiseItem(null);
+        }
+        //If Cannot Merge, Come Back
+        else
+        {
+            //Merge invalid n failed, display feedback
+        }
+    }
+}
