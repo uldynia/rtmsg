1abb6d7349f37f7d9bea2157c706d1d81579cde5
ChongChiSengVincent <60565927+TheEmeraldsPlayer@users.noreply.github.com>
2025-03-29 00:44:25 +0800

    Add comments to GameManager.cs

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -4,6 +4,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance { get; private set; } // makes it so that other scripts cant change the singleton instance
 
+    // Y boundaries of the field, allows units to be placed inversely via EntityTransform
     [SerializeField]
     private float topY;
 
@@ -14,6 +15,8 @@ public class GameManager : MonoBehaviour
     {
         instance = this;
     }
+
+    // Get Function
 
     public Vector2 GetPlaceableBoundaryY()
     {
