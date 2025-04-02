a238d10df88058ae5a5124cb099c6f46cc7c87da
TerYeeHong <yhspiritous@gmail.com>
2025-04-02 21:41:29 +0800

    milk animating

--- Assets/Scripts/Consumeables/Consumeable.cs
+++ Assets/Scripts/Consumeables/Consumeable.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class Consumeable : MonoBehaviour
 {
@@ -9,7 +10,13 @@ public class Consumeable : MonoBehaviour
     public Vector2Int coord;
 
     public bool hasBeenPicked = false;
-    
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, -22);
+        transform.DOLocalRotate(new Vector3(0, 0, 22), 2.5f).SetEase(Ease.InOutCubic).SetLoops(-1, LoopType.Yoyo);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         EntityBaseBehaviour behaviour = collision.GetComponent<EntityBaseBehaviour>();
